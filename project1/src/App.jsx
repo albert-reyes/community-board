c9f05ae1c6cd15d977bac44563a5fa3c85122315
albert-reyes <109039459+albert-reyes@users.noreply.github.com>
2024-03-01 06:37:01 +0000

    firee

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,14 +1,9 @@
-import './App.css';
-import Calendar from './components/Calender';'./components/Calendar'
+import Card from './Card.jsx'
 
-const App = () => {
+function App() {
+  return(
+    <Card></Card>
+  );
+}
 
-  return (
-    <div className="App">
-      <h1>Itinerary For 7 Days in Japan 🗾</h1>
-      <h2>Welcome to Japan, Albert! Check out this calendar to get to 
-        know the city and see all the sights during your stay.</h2><Calendar />
-    </div>
-  )
-}   
 export default App
