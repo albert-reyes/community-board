151a1b9c6f464f3fa71c9ab9cb8f4bc4aa74a1c3
albert-reyes <109039459+albert-reyes@users.noreply.github.com>
2024-03-01 04:08:58 +0000

    fire

---
+++ project1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Calendar from './components/Calender';'./components/Calendar'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Itinerary For 7 Days in Japan 🗾</h1>
+      <h2>Welcome to Japan, Albert! Check out this calendar to get to 
+        know the city and see all the sights during your stay.</h2><Calendar />
+    </div>
+  )
+}   
+export default App
